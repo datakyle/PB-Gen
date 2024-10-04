4d54a1a150a3f488746757f4229eab49b2226057
Kyle Bautista <fklbautista@gmail.com>
2024-10-04 10:03:58 -0700

    Initial Commit

---
+++ Fun Note Tester/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fun Note Tester
+//
+//  Created by Francis-Kyle Bautista on 10/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fun Note Tester/Fun_Note_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_Note_TesterApp.swift
+//  Fun Note Tester
+//
+//  Created by Francis-Kyle Bautista on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Fun_Note_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
